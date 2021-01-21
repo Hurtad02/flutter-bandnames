17387b7378c0895d2e883b1483f1b3fdd191492c
Diego Hurtado <emmanueljhm@gmail.com>
2021-01-20 20:26:20 -0400

    Status

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
 
 import 'package:band_names_app/src/models/band.dart';
+import 'package:band_names_app/src/services/socket_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
 
@@ -12,27 +15,48 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  List<Band> bands = [
-    Band(id: '1', name: 'Metallica', votes: 5),
-    Band(id: '2', name: 'AC/DC', votes: 5),
-    Band(id: '3', name: 'Guns n Roses', votes: 5),
-    Band(id: '4', name: 'Bon Jovi', votes: 5),
-    Band(id: '5 ', name: 'Scorpions', votes: 5),
-  ];
+  List<Band> bands = [];
+
+  @override
+  void initState() {
+    final socketService = Provider.of<SocketService>(context, listen: false);
+    socketService.socket.on('active-bands', _handleActiveBands);
+    super.initState();
+  }
+
+  _handleActiveBands(dynamic payload){
+    this.bands = (payload as List).map((band) => Band.fromMap(band)).toList();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
+
+    final socketService = Provider.of<SocketService>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('BandNames', style: TextStyle(color: Colors.black87),),
         backgroundColor: Colors.white,
         elevation: 1,
+        actions: <Widget>[
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: (socketService.serverStatus == ServerStatus.Online)
+            ? Icon(Icons.check_circle, color: Colors.blue[300],)
+            : Icon(Icons.offline_bolt, color: Colors.red,),
+          )
+        ],
       ),
-      body: ListView.builder(
-        itemCount: bands.length,
-        itemBuilder: (context, i) {
-        return _bandTile(bands[i]);
-      },
+      body: Column(
+        children: <Widget>[
+          _showGraph(),
+          Expanded(
+            child: ListView.builder(
+              itemCount: bands.length,
+              itemBuilder: (context, i) => _bandTile(bands[i]),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
@@ -43,10 +67,13 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget _bandTile(Band band){
+    
+    final socketService = Provider.of<SocketService>(context, listen: false);
+    
     return Dismissible(
       direction: DismissDirection.startToEnd,
-      onDismissed: (direction){
-      //  TODO: llamar el borrado en el server
+      onDismissed: (_){
+        socketService.socket.emit('delete.band', {'id': band.id});
       },
       background: Container(
         padding: EdgeInsets.only(left: 8.0),
@@ -64,8 +91,7 @@ class _HomePageState extends State<HomePage> {
         ),
         title: Text(band.name),
         trailing: Text('${band.votes}', style: TextStyle(fontSize: 20),),
-        onTap: (){
-        },
+        onTap: () => socketService.socket.emit('vote-band', {'id': band.id}),
       ),
     );
   }
@@ -77,7 +103,7 @@ class _HomePageState extends State<HomePage> {
       // Android
       return showDialog(
           context: context,
-          builder: (context){
+          builder: (_){
             return AlertDialog(
               title: Text('New band name:'),
               content: TextField(
@@ -122,12 +148,23 @@ class _HomePageState extends State<HomePage> {
   }
 
   void addBandToList(String name){
+    final socketService = Provider.of<SocketService>(context, listen: false);
     if (name.length > 1){
-    //  Podemos agregar
-      this.bands.add(new Band(id: DateTime.now().toString(), name: name, votes: 0));
-      setState(() {});
+    //  Emitir: add-band {name: name}
+      socketService.socket.emit('add-band', {'name': name});
     }
     Navigator.pop(context);
   }
 
+  Widget _showGraph(){
+    Map<String, double> dataMap = new Map();
+      bands.forEach((band) {
+        dataMap.putIfAbsent(band.name, () => band.votes.toDouble());
+      });
+    return Container(
+        width: double.infinity,
+        height: 200.0,
+        child: PieChart(dataMap: dataMap)
+    );
+  }
 }
--- lib/src/services/socket_service.dart
+++ lib/src/services/socket_service.dart
@@ -7,9 +7,14 @@ enum ServerStatus {
   Offline,
   Connecting
 }
+
 class SocketService with ChangeNotifier{
 
   ServerStatus _serverStatus = ServerStatus.Connecting;
+  IO.Socket _socket;
+
+  ServerStatus get serverStatus => this._serverStatus;
+  IO.Socket get socket => this._socket;
 
   SocketService(){
     this._initConfig();
@@ -18,14 +23,18 @@ class SocketService with ChangeNotifier{
   void _initConfig(){
 
     // Dart client
-    IO.Socket socket = IO.io('http://localhost:3000', {
+    _socket = IO.io('http://190.205.103.213:3000', {
       'transports': ['websocket'],
       'autoConnect': true,
     });
-    socket.onConnect((_) {
-      print('connect');
+    _socket.on('connect', (_) {
+      this._serverStatus = ServerStatus.Online;
+      notifyListeners();
     });
-    socket.onDisconnect((_) => print('disconnect'));
+    _socket.on('disconnect', (_) {
+      this._serverStatus = ServerStatus.Offline;
+      notifyListeners();
+    });
   }
 
 }
